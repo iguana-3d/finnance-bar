e91d2e1201fb60dffff6318ee41330aa7f6b129e
iguana3d <igor.cordeiro2016@outlook.com>
2023-02-02 18:50:44 -0300

    feature: dollar currency implemented

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const fetchDollarCurrency = async () => {
+  const url = 'https://economia.awesomeapi.com.br/last/USD-BRL';
+
+  const fetchURL = await fetch(url);
+  const data = await fetchURL.json();
+  return data.USDBRL;
+};
+
 const fetchMarketShare = async () => {
   const url =
     'https://brapi.dev/api/quote/VALE3,ABEV3,PETR4,TOTS3,RAIZ4,ITUB4,ELET3,MGLU3,AMER3,B3SA3,BBAS3,BBDC4,PRIO3,BPAC11,RENT3,PETR3,HAPV3,ASAI3,RAIL3,INBR32,NTCO3,GGBR4';
@@ -11,8 +19,30 @@ const financeContainer = document.querySelector(
   '.financebar-custom__container'
 );
 
+const financeDolarContainer = document.querySelector(
+  '.finance-custom__dolar-container'
+);
+
+const createDollarDiv = async () => {
+  const dollarPrice = await fetchDollarCurrency();
+
+  console.log(dollarPrice);
+  const dollarPriceData = `
+    <div class="finance-custom__dolar-content">
+      <span class="financebar-custom__dolar-icon">D</span>
+      <span class="financebar-custom__dolar-currency"> - Dólar</span>
+      <span class="financebar-custom__dolar-price"> ${parseFloat(
+        dollarPrice.ask
+      ).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</span>
+    </div>
+    <span class="financebar-custom__dolar-bar"></span>
+    `;
+
+  financeDolarContainer.innerHTML = dollarPriceData;
+};
+
 const marketShared = async () => {
-  let marketShareData = await fetchMarketShare();
+  const marketShareData = await fetchMarketShare();
 
   const createMarketShare = marketShareData
     .map((asset, index) => {
@@ -45,6 +75,7 @@ const marketShared = async () => {
   financeContainer.innerHTML = createMarketShare;
 };
 
+createDollarDiv();
 marketShared().then(() => {
   $(document)
     .ready(function () {
@@ -53,10 +84,11 @@ marketShared().then(() => {
         autoplay: true,
         autoplaySpeed: 0,
         cssEase: 'linear',
+        dots: false,
         variableWidth: true,
         draggable: false,
         centerPadding: '10px',
-        arrows: true,
+        arrows: false,
         pauseOnHover: false,
         pauseOnFocus: false,
       });
